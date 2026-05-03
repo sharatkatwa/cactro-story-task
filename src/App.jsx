fa636e82bc8b058a8bf7fa54a3ef0eff31ff5534
sharatkatwa <sjkatwa2000@gmail.com>
2026-05-03 11:20:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-full'>
+    {/* story container */}
+      <div className=''></div>
+    </div>
+  )
+}
+
+export default App
